6d80d82c3df688fe4c0098b8f6b26ae4b34904c0
MXFire1906 <85999078+ManuelDogbatse@users.noreply.github.com>
2024-01-07 11:59:39 +0000

    Added SSH server interface

--- honeypot.py
+++ honeypot.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
-import socket
+import socket       # Library for network sockets
+import paramiko     # Library for SSH server
 
-HOST = "127.0.0.1" # Loopback address
+HOST = "127.0.0.1"
 PORT = 2222
+
+    ## Send message to client
+    #client_sock.send(b"Hello!\n")
+    ## Read message from client
+    #print(client_sock.recv(256).decode())
+
 
 def main():
     # Make a TCP socket that communicates via IP addresses
@@ -16,10 +23,15 @@ def main():
     # Get connected client's IP address and port number
     client_sock, (client_ip, client_port) = server_sock.accept()
     print(f"Connection from {client_ip}:{client_port}")
-    # Send message to client
-    client_sock.send(b"Hello!\n")
-    # read message from client
-    print(client_sock.recv(256).decode())
+    # Create an SSH session over client socket
+    transport = paramiko.Transport(client_sock)
+    # Generate key required for SSH server and add key to SSH session
+    server_key = paramiko.RSAKey.generate(2048)
+    transport.add_server_key(server_key)
+    # Create SSH server interface
+    ssh = paramiko.ServerInterface()
+    # Start server with SSH server interface
+    transport.start_server(server=ssh)
 
 if __name__ == "__main__":
     main()
