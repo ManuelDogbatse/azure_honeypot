e5f27ae22a25f356d891a25857845d89c8315a45
MXFire1906 <85999078+ManuelDogbatse@users.noreply.github.com>
2024-01-08 14:32:01 +0000

    Configured server interface

--- honeypot.py
+++ honeypot.py
@@ -1,59 +1,71 @@
 #!/usr/bin/env python3
 
+import sys
 import socket       # Network sockets
 import paramiko     # SSH server
 import threading    # Multithreading
 import logging
 from eprint import eprint
+from binascii import hexlify
 
+# Constant variables
 HOST = "127.0.0.1"
 PORT = 2222
 SERVER_KEY = paramiko.RSAKey(filename="server_key")
+SSH_BANNER = "SSH-2.0-OpenSSH_8.2p1 Ubuntu-4ubuntu0.1"
 
-# Logging format
-# asctime: Current time
-# 
+# Setting up logging format for paramiko
 logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=logging.INFO,
-        )
-        #filename="ssh_honeypot.log")
+        #format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        level=logging.INFO)
+        ##filename="ssh_honeypot.log")
 
 # Defining honeypot attributes
 class HoneypotServer(paramiko.ServerInterface):
-
+    # Get client's IP address and port number
     def __init__(self, client_addr):
         (self.client_ip, self.client_port) = client_addr
 
-    def check_channel_request(self, kind, chanid):
-        logging.info(f"client called check_channel_request({self.client_ip}:{self.client_port}): {kind}")
-        if kind == "session":
-            return paramiko.OPEN_SUCCEEDED
+    # Defining server interface methods
+    # BEGIN
+    #def check_channel_request(self, kind, chanid):
+    #    logging.info(f"client called check_channel_request({self.client_ip}:{self.client_port}): {kind}")
+    #    if kind == "session":
+    #        return paramiko.OPEN_SUCCEEDED
 
     def get_allowed_auths(self, username):
-        logging.info(f"client called get_allowed_auths ({self.client_ip}:{self.client_port}) with username {username}")
+        #logging.info(f"client requesting auth methods ({self.client_ip}:{self.client_port}): username {username}")
         return "publickey,password"
 
-    #def check_auth_publickey(self, username, key):
+    def check_auth_none(self, username):
+        logging.info(f"no auth login attempt ({self.client_ip}:{self.client_port}: username: {username})")
 
+    def check_auth_publickey(self, username, key):
+        fingerprint = hexlify(key.get_fingerprint())
+        logging.info(f"public key auth attempt ({self.client_ip}:{self.client_port}): username: {username}, key name {key.get_name()}, md5 fingerprint: {fingerprint}, base64: {key.get_base64()}, bits: {key.get_bits()}")
+        return paramiko.AUTH_FAILED
 
     # Check login attempt using password, and log the username and password used
     def check_auth_password(self, username, password):
         logging.info(f"login attempt ({self.client_ip}:{self.client_port}): username: {username}, password: {password}")
         return paramiko.AUTH_FAILED
+    # END
 
 def handle_connection(client_sock, client_addr):
+    logging.info(f"new client connection: {client_addr[0]}:{client_addr[1]}")
     try:
         # Create an SSH session over client socket
         transport = paramiko.Transport(client_sock)
         # Retrieve pre-generated key from file
         transport.add_server_key(SERVER_KEY)
+        # Change the banner to appear more convincing
+        transport.local_version = SSH_BANNER
         # Create SSH server interface
         ssh = HoneypotServer(client_addr)
         try:
             # Start server with SSH server interface
             transport.start_server(server=ssh)
-#        except paramiko.ssh_exception.SSHException:
         except Exception as err:
             eprint("*** SSH negotiation failed")
             raise Exception("SSH negotiation failed")
@@ -75,6 +87,7 @@ def main():
     except Exception as err:
         eprint(f"*** Bind failed: {err}")
         traceback.print_exc()
+        sys.exit(1)
 
     threads = []
     # While loop to keep accepting connections when client socket disconnects
