b02248b33f10a93bc2ef8111008ae2a0eff752fb
MXFire1906 <85999078+ManuelDogbatse@users.noreply.github.com>
2024-01-12 18:00:15 +0000

    Added formatting to functions

--- format_logs.sh
+++ format_logs.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 
+# awk functions
+# Trim space in awk function
+TRIM_SPACE_COMMAND='{for (i=1; i <= NF; i++) {
+    gsub(/^[ \t]+/,"",$i)
+    gsub(/[ \t]+$/,"",$i)
+} }'
+# Print all columns in awk function
+PRINT_COLUMNS_COMMAND='{for (i=1; i <= NF; i++) {
+    print $i
+} }'
+AWK_COMMAND="$TRIM_SPACE_COMMAND $PRINT_COLUMNS_COMMAND"
+
+# Regular expressions for log values
+REGEXP_STD="(?<=\:\s).*$"
+REGEXP_INPUT="(?<=').*(?='$)"
+
 # Log arrays
-declare -a connection_logs=()
-declare -a passwd_auth_logs=()
-declare -a pub_key_auth_logs=()
+declare -a geo_logs=()
+declare -a uname_logs=()
+declare -a passwd_logs=()
+declare -a pub_key_logs=()
+declare -a auth_type_logs=()
+
+# Format geolocation logs
+format_geo_logs() {
+    # Split log by '--' and store each line as an item in an array
+    IFS=$'\n' read -r -d '' -a geo_log_arr < <(echo "$1" | awk -F '--' "$AWK_COMMAND")
+    echo "Time: ${geo_log_arr[0]}"
+    echo "Message: ${geo_log_arr[2]}"
+    geo_log_arr[3]=$(echo "${geo_log_arr[3]}" | grep -Po "$REGEXP_STD")
+    echo "IP Address: ${geo_log_arr[3]}"
+    geo_log_arr[5]=$(echo "${geo_log_arr[5]}" | grep -Po "$REGEXP_INPUT")
+    echo "Username: ${geo_log_arr[5]}"
+}
 
 # Place desired logs in respective arrays
 get_ssh_logs() {
@@ -12,36 +42,16 @@ get_ssh_logs() {
         # Get new connection logs
         if [[ "$line" =~ .*"no auth attempt".* ]];
         then
-            connection_logs+=("$line")
+            format_geo_logs "$line"
         elif [[ "$line" =~ .*"password auth attempt".* ]];
         then
+            declare -a passwd_auth_logs=()
             passwd_auth_logs+=("$line")
         elif [[ "$line" =~ .*"public key auth attempt".* ]];
         then
+            declare -a pub_key_auth_logs=()
             pub_key_auth_logs+=("$line")
         fi
     done < "$1"
 }
 get_ssh_logs "./ssh_honeypot.log"
-
-geo_log="${connection_logs[0]}"
-# Trim space in awk function
-trim_space='{for (i=1; i <= NF; i++) {
-    gsub(/^[ \t]+/,"",$i)
-    gsub(/[ \t]+$/,"",$i)
-} }'
-# Print all columns in awk function
-print_columns='{for (i=1; i <= NF; i++) {
-    print $i
-} }'
-geo_log_awk="$trim_space $print_columns"
-IFS=$'\n' read -r -d '' -a geo_log_arr < <(echo "$geo_log" | awk -F '--' "$geo_log_awk")
-#geo_log_arr=( $(echo "$geo_log" | awk -F '--' "$geo_log_awk") ) 
-echo "Arr: ${geo_log_arr[0]}"
-#echo "Message: $msg"
-#passwd_log="${passwd_auth_logs[0]}"
-#passwd_log_awk="$trim_space $print_all"
-#echo "$passwd_log" | awk -F '--' "$passwd_log_awk"
-#pub_key_log="${pub_key_auth_logs[0]}"
-#pub_key_log_awk="$trim_space $print_all"
-#echo "$pub_key_log" | awk -F '--' "$pub_key_log_awk"
